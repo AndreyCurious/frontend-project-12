c7b4b01166069ee1ace3007113f0dcab3fd8bfac
Andrey Didenko <ded14181998@gmail.com>
2023-07-23 16:31:55 +0500

    fix for tests

--- frontend/src/init.jsx
+++ frontend/src/init.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import i18next from 'i18next';
-import { Provider as ProviderRollbar } from '@rollbar/react';
+import { Provider as RollbarProv, ErrorBoundary } from '@rollbar/react';
 import { I18nextProvider, initReactI18next } from 'react-i18next';
 import { Provider } from 'react-redux';
-import Rollbar from 'rollbar';
 import App from './components/App';
 import resources from './locales/index.js';
 import store from './slices/index.js';
@@ -12,7 +11,14 @@ const rollbarConfig = {
   accessToken: process.env.REACT_APP_ROLLBAR_TOKEN,
   environment: 'production',
 };
-const rollbar = new Rollbar(rollbarConfig);
+
+const RollbarProvider = ({ children }) => (
+  <RollbarProv config={rollbarConfig}>
+    <ErrorBoundary>
+      { children }
+    </ErrorBoundary>
+  </RollbarProv>
+);
 
 const init = async () => {
   const i18n = i18next.createInstance();
@@ -25,13 +31,13 @@ const init = async () => {
     });
 
   return (
-    <ProviderRollbar instance={rollbar}>
+    <RollbarProvider>
       <Provider store={store}>
         <I18nextProvider i18n={i18n}>
           <App />
         </I18nextProvider>
       </Provider>
-    </ProviderRollbar>
+    </RollbarProvider>
   );
 };
 
