aa1ac455e45182c0702b4c6a26bec9062b92dd27
Andrey Didenko <ded14181998@gmail.com>
2023-07-23 16:47:25 +0500

    rename navbar

--- frontend/src/components/UI/navbar.jsx
+++ frontend/src/components/UI/navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
         <a className="navbar-brand" href="/">
           <img src="https://upload.wikimedia.org/wikipedia/commons/3/3d/DC_Comics_logo.svg" alt="logo" width="30" height="30" className='d-inline-blick align-top'/>
           {' '}
-          Digital Chat
+          Hexlet Chat
         </a>
         {auth.user ? <button onClick={auth.logOut} className='btn btn-primary'>{t('login.logout')}</button> : null}
       </div>
